83e542ac607bb58a9b4a58f9b6d053825d22697a
Kristijan Baro <baro.kristijan@gmail.com>
2022-05-26 14:51:13 +0200

    spawn grids at intervals

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ class Grid {
 
 const player = new Player()
 const projectiles = []
-const grids = [new Grid()]
+const grids = []
 const keys = {
     a: {
         pressed: false
@@ -182,6 +182,9 @@ const keys = {
         pressed: false
     }
 }
+
+let frames = 0
+let randomInterval = Math.floor(Math.random() * 500 + 500)
 
 
 function animate() {
@@ -218,6 +221,15 @@ function animate() {
         player.velocity.x = 0
         player.rotation = 0
     }
+
+    // spawning enemies
+    if (frames % randomInterval === 0) {
+        grids.push(new Grid())
+        randomInterval = Math.floor(Math.random() * 500 + 500) 
+        frames = 0
+    }
+
+    frames++
 }
 
 animate()
